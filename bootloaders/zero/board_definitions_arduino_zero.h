52257ce0b2c59c9e659d709595af022ddc09c7c0
matt venn <matt@mattvenn.net>
2019-04-17 19:51:46 +0200

    add manufacture and fix vid and pid

--- bootloaders/zero/board_definitions_arduino_zero.h
+++ bootloaders/zero/board_definitions_arduino_zero.h
@@ -24,10 +24,11 @@
  * USB device definitions
  */
 #define STRING_PRODUCT "Raise Bootloader"
-#define USB_VID_HIGH   0x23
-#define USB_VID_LOW    0x41
-#define USB_PID_HIGH   0x00
-#define USB_PID_LOW    0x4D
+#define STRING_MANUFACTURER "Dygma"
+#define USB_VID_HIGH   0x12
+#define USB_VID_LOW    0x09
+#define USB_PID_HIGH   0x22
+#define USB_PID_LOW    0x00
 
 /*
  * If BOOT_DOUBLE_TAP_ADDRESS is defined the bootloader is started by
